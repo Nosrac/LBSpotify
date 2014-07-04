e57b57f555867b6c323dbd632f44ae8b028d0945
Kyle Carson <elyknosrac@gmail.com>
2014-07-04 15:02:35 -0500

    Show Albums and Artists as a top result

--- Search Spotify.lbaction/Contents/Scripts/suggestion.php
+++ Search Spotify.lbaction/Contents/Scripts/suggestion.php
@@ -1,18 +1,15 @@
 <?
-function findSuggestions($search, $max = 10)
+class SpotifyQuery
 {
-	$search = strtolower(trim($search));
-
-	if (strlen($search) == 0) return [];
-
-	$result = json_decode(file_get_contents('http://ws.spotify.com/search/1/track.json?q=' . urlencode($search)));
-
-	$suggestions = [];
-	$urls = [];
-
-	foreach($result->tracks as $track)
+	var $search;
+	function __construct($search)
 	{
-		$suggestions[] = [
+		$this->search = strtolower(trim($search));
+	}
+
+	function suggestionForTrack($track)
+	{
+		return [
 			'title' => $track->name . ' by ' . $track->artists[0]->name,
 			'icon' => "at.obdev.LaunchBar:AudioTrackTemplate",
 			'url' => $track->href,
@@ -22,25 +19,97 @@ function findSuggestions($search, $max = 10)
 			// 		'icon' => "at.obdev.LaunchBar:AudioTrackTemplate",
 			// 		'url' => $track->href,
 			// 	],
-			// 	[
-			// 		'title' => $artistText,
-			// 		'icon' => "at.obdev.LaunchBar:ArtistTemplate",
-			// 		'url' => $track->artists[0]->href,
-			// 	],
-			// 	[
-			// 		'title' => $albumText,
-			// 		'icon' => "at.obdev.LaunchBar:AlbumTemplate",
-			// 		'url' => $track->album->href,
-			// 	]
+			// 	$this->suggestionForArtist($track->artists[0]),
+			// 	$this->suggestionForAlbum($track->album)
 			// ]
 		];
 	}
 
-	$suggestions = array_slice($suggestions, 0, $max);
-	return $suggestions;
+	function suggestionForArtist($artist)
+	{
+		return [
+					'title' => $artist->name,
+					'icon' => "at.obdev.LaunchBar:ArtistTemplate",
+					'url' => $artist->href,
+				];
+	}
+
+	function suggestionForAlbum($album)
+	{
+		return [
+					'title' => $album->name,
+					'icon' => "at.obdev.LaunchBar:AlbumTemplate",
+					'url' => $album->href,
+				];
+	}
+
+	function suggestions($max = 10)
+	{
+		if (strlen($this->search) == 0) return [];
+
+		$result = json_decode(file_get_contents('http://ws.spotify.com/search/1/track.json?q=' . urlencode($this->search)));
+
+		if (count($result->tracks) == 0) return [];
+
+		$suggestions = [ $this->determinePrimaryResult( $result->tracks[0] ) ];
+		foreach($result->tracks as $track)
+		{
+			$suggestions[] = $this->suggestionForTrack($track);
+		}
+
+		$suggestions = array_filter($suggestions);
+
+		$suggestions = array_slice($suggestions, 0, $max);
+
+		return $suggestions;
+	}
+
+	function determinePrimaryResult($track)
+	{
+		$results = [
+			[
+				'title' => $track->name,
+				'url' => $track->href,
+				'value' => false,
+			], [
+				'title' => $track->artists[0]->name,
+				'url' => $track->artists[0]->href,
+				'value' => $this->suggestionForArtist( $track->artists[0] ),
+			], [
+				'title' => $track->album->name,
+				'url' => $track->album->href,
+				'value' => $this->suggestionForAlbum( $track->album ),
+			],
+		];
+
+		usort($results, function($a, $b)
+		{
+		    $aVal = levenshtein($a['title'], $this->search, 10, 10, 2);
+		    $bVal = levenshtein($b['title'], $this->search, 10, 10, 2);
+
+		    if ($aVal < $bVal) return -1;
+		    if ($aVal > $bVal) return 1;
+
+		    if ($a['value'] && ! $b['value'])
+		    {
+		    	return -1;
+		    } else if ($b['value'] && ! $a['value'])
+		    {
+		    	return 1;
+		    }
+
+		    return 0;
+		});
+
+		$primaryResult = $results[0];
+
+		return $primaryResult['value'];
+	}
 }
 
-$suggestions = findSuggestions( $argv[1] );
+$query = new SpotifyQuery( $argv[1] );
+
+$suggestions = $query->suggestions();
 
 echo json_encode($suggestions);
 
