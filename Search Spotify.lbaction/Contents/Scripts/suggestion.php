8b54a1793b23630f2398736728ac65ba3f99c2cd
Kyle Carson <elyknosrac@gmail.com>
2014-07-04 16:53:34 -0500

    Begin Caching Results, display more artists/albums

--- Search Spotify.lbaction/Contents/Scripts/suggestion.php
+++ Search Spotify.lbaction/Contents/Scripts/suggestion.php
@@ -11,6 +11,7 @@ class SpotifyQuery
 	{
 		return [
 			'title' => $track->name . ' by ' . $track->artists[0]->name,
+			'original_title' => $track->name,
 			'icon' => "at.obdev.LaunchBar:AudioTrackTemplate",
 			'url' => $track->href,
 			// 'children' => [
@@ -29,6 +30,7 @@ class SpotifyQuery
 	{
 		return [
 					'title' => $artist->name,
+					'original_title' => $artist->name,
 					'icon' => "at.obdev.LaunchBar:ArtistTemplate",
 					'url' => $artist->href,
 				];
@@ -38,31 +40,104 @@ class SpotifyQuery
 	{
 		return [
 					'title' => $album->name,
+					'original_title' => $album->name,
 					'icon' => "at.obdev.LaunchBar:AlbumTemplate",
 					'url' => $album->href,
 				];
 	}
 
-	function suggestions($max = 10)
+	function sortSuggestions($suggestions)
 	{
+		foreach($suggestions as $i => $suggestion)
+		{
+			$position = stripos( strtolower($suggestion['original_title']), $this->search );
+			if ($position === 0)
+			{
+				$suggestions[$i]['score'] = 0;
+
+			} else {
+				$suggestions[$i]['score'] = $i + 1;
+			}
+		}
+
+		usort($suggestions, function($a, $b)
+		{
+		    $aVal = $a['score'];
+		    $bVal = $b['score'];
+
+		    if ($aVal < $bVal) return -1;
+		    if ($aVal > $bVal) return 1;
+		    return 0;
+		});
+
+		return $suggestions;
+	}
+
+	function suggestions($max = 15)
+	{
+		$cachekey = "suggestions_" . $this->search;
+
+		$cache = $this->getCache($cachekey);
+		if ($cache) return $cache;
+
 		if (strlen($this->search) == 0) return [];
 
 		$result = json_decode(file_get_contents('http://ws.spotify.com/search/1/track.json?q=' . urlencode($this->search)));
 
 		if (count($result->tracks) == 0) return [];
 
-		$suggestions = [ $this->determinePrimaryResult( $result->tracks[0] ) ];
+		$suggestions = [ ];
 		foreach($result->tracks as $track)
 		{
-			$suggestions[] = $this->suggestionForTrack($track);
+			$extras = [ $this->suggestionForArtist( $track->artists[0] ), $this->suggestionForAlbum( $track->album ) ];
+			foreach( $extras as $result)
+			{
+				$adjustedTitle = preg_replace('/[^a-zA-Z0-9]+/', '', $result['title']);
+				$adjustedSearch = preg_replace('/[^a-zA-Z0-9]+/', '', $this->search);
+
+				if (stripos( $adjustedTitle, $adjustedSearch) !== false)
+				{
+					$suggestions[] = $result;
+				}
+			}
+			$suggestions[] = $this->suggestionForTrack( $track );
 		}
 
-		$suggestions = array_filter($suggestions);
+		$suggestions =  array_filter($suggestions);
+
+		$suggestions = $this->sortSuggestions($suggestions);
+
+		$suggestions = $this->removeDuplicateSuggestions($suggestions);
 
 		$suggestions = array_slice($suggestions, 0, $max);
 
+		$cache = $this->setCacheValue($cachekey, $suggestions);
+
 		return $suggestions;
 	}
+
+	function removeDuplicateSuggestions($suggestions)
+	{
+		foreach($suggestions as $i => $suggestion)
+		{
+			if (! $suggestion)
+			{
+				unset($suggestions[$i]);
+				continue;
+			}
+			for($j = 0; $j < $i; $j++)
+			{
+				if (! isSet($suggestions[$j])) continue;
+				if ($suggestion['url'] == $suggestions[$j]['url'])
+				{
+					unset($suggestions[$i]);
+					break;
+				}
+			}
+		}
+		return array_values($suggestions);
+	}
+
 
 	function determinePrimaryResult($track)
 	{
@@ -105,6 +180,55 @@ class SpotifyQuery
 
 		return $primaryResult['value'];
 	}
+
+	///////////
+	/////////// Cache
+	///////////
+
+	private $cacheFile = "/tmp/LBSpotifyCache.json";
+
+	function getCache($key = false)
+	{
+		$cache = [];
+		if (file_exists( $this->cacheFile ))
+		{
+			$cache = json_decode( file_get_contents( $this->cacheFile), true );
+		}
+		if (! is_array($cache))
+		{
+			$cache = [];
+		}
+
+		if ($key)
+		{
+			if (isSet($cache[$key]))
+			{
+				return $cache[$key];
+			}
+			return null;
+		}
+
+		return $cache;
+	}
+
+	function setCache($cache)
+	{
+		if (! is_array($cache)) return;
+
+		file_put_contents( $this->cacheFile, json_encode( $cache ) );
+	}
+
+	function setCacheValue($key, $value)
+	{
+		$cache = $this->getCache();
+		$cache[$key] = $value;
+
+		$this->setCache($cache);
+	}
+
+	///////////
+	/////////// End Cache
+	///////////
 }
 
 $query = new SpotifyQuery( $argv[1] );
