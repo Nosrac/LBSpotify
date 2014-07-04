122fb37d1ea21be722d94fa9e5fa7ebb1a582987
Kyle Carson <elyknosrac@gmail.com>
2014-07-04 14:24:32 -0500

    Remove jacky implementation of opening URLs

--- Search Spotify.lbaction/Contents/Scripts/default.js
+++ Search Spotify.lbaction/Contents/Scripts/default.js
@@ -1,20 +1,5 @@
 function runWithString(string)
 {
-	urlFile = "/tmp/LBSpotifySuggestionCache";
-
-	if (! File.exists(urlFile))
-	{
-		LaunchBar.alert("LBSpotify failed");
-		return;
-	}
-
-	var object = File.readJSON(urlFile);
-
-	if (! string in object)
-	{
-		LaunchBar.alert("LBSpotify messed up");
-		return;
-	}
-
-	LaunchBar.openURL(object[string]);
+	LaunchBar.openURL(string);
 }
+
--- Search Spotify.lbaction/Contents/Scripts/suggestion.php
+++ Search Spotify.lbaction/Contents/Scripts/suggestion.php
@@ -7,58 +7,34 @@ function findSuggestions($search, $max = 10)
 
 	$result = json_decode(file_get_contents('http://ws.spotify.com/search/1/track.json?q=' . urlencode($search)));
 
-	file_put_contents("/tmp/last.txt", json_encode($result));
-
 	$suggestions = [];
 	$urls = [];
 
-	$icon = 'com.spotify.client';
-
 	foreach($result->tracks as $track)
 	{
-		$longTitleText = $track->name . ' by ' . $track->artists[0]->name;
-
-		$urls[ $longTitleText ] = $track->href;
-		$urls[ $track->name ] = $track->href;
-
-		$artistText = 'Artist: ' . $track->artists[0]->name;
-		$urls[ $artistText ] = $track->href;
-
-		$albumText = 'Album: ' . $track->album->name;
-		$urls[ $albumText ] = $track->href;
-
 		$suggestions[] = [
-			'title' => $longTitleText,
-			'icon' => $icon,// . ":collection-songs@2x",
-			// 'action' => "open.sh",
-			// 'actionArgument' => $track->href,
+			'title' => $track->name . ' by ' . $track->artists[0]->name,
+			'icon' => "at.obdev.LaunchBar:AudioTrackTemplate",
+			'url' => $track->href,
 			// 'children' => [
 			// 	[
 			// 		'title' => $track->name,
-			// 		'icon' => $icon,
-			// 		'action' => "open.sh",
-			// 		'actionArgument' => $track->href,
+			// 		'icon' => "at.obdev.LaunchBar:AudioTrackTemplate",
+			// 		'url' => $track->href,
 			// 	],
 			// 	[
 			// 		'title' => $artistText,
-			// 		'icon' => $icon,
-			// 		'action' => "open.sh",
-			// 		'actionArgument' => $track->artists[0]->href,
+			// 		'icon' => "at.obdev.LaunchBar:ArtistTemplate",
+			// 		'url' => $track->artists[0]->href,
 			// 	],
 			// 	[
 			// 		'title' => $albumText,
-			// 		'icon' => $icon,
-			// 		'action' => "open.sh",
-			// 		'actionArgument' => $track->album->href,
+			// 		'icon' => "at.obdev.LaunchBar:AlbumTemplate",
+			// 		'url' => $track->album->href,
 			// 	]
 			// ]
 		];
 	}
-
-	$urlFile = "/tmp/LBSpotifySuggestionCache";
-	file_put_contents($urlFile, json_encode($urls));
-
-	file_put_contents("/tmp/last.txt", json_encode($suggestions));
 
 	$suggestions = array_slice($suggestions, 0, $max);
 	return $suggestions;
