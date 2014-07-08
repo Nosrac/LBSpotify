5d9291d9ab8ba8b59386dd6c7019d0aba4b9f239
Kyle Carson <elyknosrac@gmail.com>
2014-07-08 11:40:44 -0500

    Bugfix

--- Search Spotify.lbaction/Contents/Scripts/default.js
+++ Search Spotify.lbaction/Contents/Scripts/default.js
@@ -1,6 +1,17 @@
 function runWithString(string)
 {
-	string = string || "";
-	LaunchBar.openURL("spotify:search:" + encodeURIComponent(string) );
+	var url = string
+	if ( ! string.match(/^spotify:/))
+	{
+		string = string || "";
+
+		url = "spotify:search:" + encodeURIComponent(string);
+	}
+
+	LaunchBar.openURL( url );
 }
 
+function run()
+{
+	LaunchBar.openURL("spotify://")
+}
